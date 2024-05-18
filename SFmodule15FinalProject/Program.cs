8ac82a1fa7d286271e9497b4b71693ad47e508c4
Ilia <isinetov@mail.ru>
2024-05-18 22:49:50 +0400

    Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.

--- SFmodule15FinalProject/Program.cs
+++ SFmodule15FinalProject/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var students = from classroom in classes from student in classroom.Students select student;
+        return students.ToArray();
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
